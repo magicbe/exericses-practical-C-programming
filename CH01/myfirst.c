ecf903ce0dbadde9822c11d611768a24e0a1291d
magicbe <magicbe.tw@gmail.com>
2019-04-02 08:35:38 +0800

    update myfirst

---
+++ CH01/myfirst.c
@@ -0,0 +1,13 @@
+/* This is my first program */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+  int num;
+  num = 100;
+  printf("num=%d\n", num);
+  // system("PAUSE");
+  return 0;
+}
